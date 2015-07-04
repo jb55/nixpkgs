ab033e40e19c37e906ea2f4a5e8082bc6f15d10c
William Casarin <bill@casarin.me>
2015-07-04 16:42:54 -0700

    caffe: init at rev 77d66dfc
    Initial version of the Caffe deep learning framework from Berkeley

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14229,6 +14229,10 @@ let
 
   ### SCIENCE / MATH
 
+  caffe = callPackage ../applications/science/math/caffe {
+    cudaSupport = config.caffe.cudaSupport or config.cudaSupport or true;
+  };
+
   ecm = callPackage ../applications/science/math/ecm { };
 
   eukleides = callPackage ../applications/science/math/eukleides {
