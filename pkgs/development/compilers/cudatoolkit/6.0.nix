170fa6b30b75e73cb23969abaed36b12c785beb3
William Casarin <bill@casarin.me>
2015-07-04 17:05:16 -0700

    remove some cruft

--- pkgs/development/compilers/cudatoolkit/6.0.nix
+++ pkgs/development/compilers/cudatoolkit/6.0.nix
@@ -3,5 +3,4 @@
 callPackage ./generic.nix (args // rec {
   version = "6.0.37";
   sha256 = "991e436c7a6c94ec67cf44204d136adfef87baa3ded270544fa211179779bc40";
-  python = python26;
 })
--- pkgs/development/compilers/cudatoolkit/7.0.nix
+++ pkgs/development/compilers/cudatoolkit/7.0.nix
@@ -4,5 +4,4 @@ callPackage ./generic.nix (args // rec {
   version = "7.0.28";
   sha256 = "1km5hpiimx11jcazg0h3mjzk220klwahs2vfqhjavpds5ff2wafi";
   url = "http://developer.download.nvidia.com/compute/cuda/7_0/Prod/local_installers/cuda_7.0.28_linux.run";
-  python = python26;
 })
