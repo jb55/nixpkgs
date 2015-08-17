afbb7efd578bb25aa81240734757aa422a056305
William Casarin <bill@casarin.me>
2015-08-16 20:05:14 -0700

    antimicro: init at 2.18
    antimicro is a graphical program used to map keyboard keys and mouse controls
    to a gamepad. Useful for playing PC games with no gamepad or poor gamepad
    support.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14587,6 +14587,8 @@ let
 
   ### MISC
 
+  antimicro = callPackage ../tools/misc/antimicro { };
+
   atari800 = callPackage ../misc/emulators/atari800 { };
 
   ataripp = callPackage ../misc/emulators/atari++ { };
