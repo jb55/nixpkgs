1aafd3eb9a5b5ef1e64ea9c102ba8159c1138832
William Casarin <bill@casarin.me>
2017-03-25 10:04:53 -0700

    rss2email: init at 3.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17673,6 +17673,8 @@ with pkgs;
 
   rss-glx = callPackage ../misc/screensavers/rss-glx { };
 
+  rss2email = python3Packages.rss2email;
+
   runit = callPackage ../tools/system/runit { };
 
   refind = callPackage ../tools/bootloaders/refind { };
