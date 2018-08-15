899764f20753f971156c565a79b3d59d6abffb43
William Casarin <jb55@jb55.com>
2018-08-14 21:56:44 -0700

    lastpass-cli: add gui pinentry support
    Currently off by default
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- pkgs/tools/security/lastpass-cli/default.nix
+++ pkgs/tools/security/lastpass-cli/default.nix
@@ -1,6 +1,13 @@
 { stdenv, lib, fetchFromGitHub, asciidoc, cmake, docbook_xsl, pkgconfig
-, bash-completion, openssl, curl, libxml2, libxslt }:
+, bash-completion, makeWrapper, openssl, curl, libxml2, libxslt, pinentry_gtk2
+, pinentryPath ? null, guiSupport ? false }:
 
+let
+  pinpath =
+    if pinentryPath == null
+      then "${pinentry_gtk2}/bin/pinentry"
+      else pinentryPath;
+in
 stdenv.mkDerivation rec {
   pname = "lastpass-cli";
   version = "1.3.3";
@@ -12,7 +19,7 @@ stdenv.mkDerivation rec {
     sha256 = "168jg8kjbylfgalhicn0llbykd7kdc9id2989gg0nxlgmnvzl58a";
   };
 
-  nativeBuildInputs = [ asciidoc cmake docbook_xsl pkgconfig ];
+  nativeBuildInputs = [ asciidoc cmake docbook_xsl pkgconfig makeWrapper ];
 
   buildInputs = [
     bash-completion curl openssl libxml2 libxslt
@@ -25,7 +32,10 @@ stdenv.mkDerivation rec {
   postInstall = ''
     install -Dm644 -T ../contrib/lpass_zsh_completion $out/share/zsh/site-functions/_lpass
     install -Dm644 -T ../contrib/completions-lpass.fish $out/share/fish/vendor_completions.d/lpass.fish
-  '';
+  '' +
+  (if guiSupport then ''
+    wrapProgram $out/bin/lpass --set LPASS_PINENTRY "${pinpath}"
+  '' else "");
 
   meta = with lib; {
     description = "Stores, retrieves, generates, and synchronizes passwords securely";
