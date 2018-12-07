871b456843fd932b618be18bf1f4abc3c49a8074
William Casarin <jb55@jb55.com>
2018-12-07 15:42:54 -0800

    nixos/bitcoind: allow multiple network services
    Signed-off-by: William Casarin <jb55@jb55.com>

--- nixos/modules/misc/ids.nix
+++ nixos/modules/misc/ids.nix
@@ -337,7 +337,7 @@
       alerta = 310;
       minetest = 311;
       rss2email = 312;
-      bitcoin = 333;
+      bitcoin = 313;
 
       # When adding a uid, make sure it doesn't match an existing gid. And don't use uids above 399!
 
@@ -635,7 +635,7 @@
       alerta = 310;
       minetest = 311;
       rss2email = 312;
-      bitcoin = 333;
+      bitcoin = 313;
 
       # When adding a gid, make sure it doesn't match an existing
       # uid. Users and groups with the same name should have equal
--- nixos/modules/services/networking/bitcoind.nix
+++ nixos/modules/services/networking/bitcoind.nix
@@ -4,8 +4,8 @@ with lib;
 
 let
   cfg = config.services.bitcoind;
-  pidFile = "${cfg.dataDir}/bitcoind.pid";
-  configFile = pkgs.writeText "bitcoin.conf" ''
+  pidFile = cfg: "${cfg.dataDir}/bitcoind.pid";
+  configFile = cfg: pkgs.writeText "bitcoin.conf" ''
     ${optionalString cfg.testnet "testnet=1"}
     ${optionalString (cfg.dbCache != null) "dbcache=${toString cfg.dbCache}"}
     ${optionalString (cfg.prune != null) "prune=${toString cfg.prune}"}
@@ -23,10 +23,10 @@ let
     # Extra config options (from bitcoind nixos service)
     ${cfg.extraConfig}
   '';
-  cmdlineOptions = escapeShellArgs [
-    "-conf=${configFile}"
+  cmdlineOptions = cfg: escapeShellArgs [
+    "-conf=${configFile cfg}"
     "-datadir=${cfg.dataDir}"
-    "-pid=${pidFile}"
+    "-pid=${pidFile cfg}"
   ];
   hexStr = types.strMatching "[0-9a-f]+";
   rpcUserOpts = { name, ... }: {
@@ -51,11 +51,53 @@ let
       name = mkDefault name;
     };
   };
+
+  mkNode = networkCfg: network: {
+    description = "Bitcoin ${network} daemon";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    preStart = ''
+      if ! test -e ${networkCfg.dataDir}; then
+        mkdir -m 0770 -p '${networkCfg.dataDir}'
+        chown '${cfg.user}:${cfg.group}' '${networkCfg.dataDir}'
+      fi
+      ln -sf '${configFile networkCfg}' '${networkCfg.dataDir}/bitcoin.conf'
+    '';
+    serviceConfig = {
+      User = cfg.user;
+      Group = cfg.group;
+      ExecStart = "${cfg.package}/bin/bitcoind ${cmdlineOptions networkCfg}";
+      StateDirectory = "bitcoind";
+      PIDFile = pidFile networkCfg;
+      Restart = "on-failure";
+
+      # Hardening measures
+      PrivateTmp = "true";
+      ProtectSystem = "full";
+      NoNewPrivileges = "true";
+      PrivateDevices = "true";
+      MemoryDenyWriteExecute = "true";
+
+      # Permission for preStart
+      PermissionsStartOnly = "true";
+  };
+  };
 in {
   options = {
 
     services.bitcoind = {
-      enable = mkEnableOption "Bitcoin daemon";
+
+      user = mkOption {
+        type = types.str;
+        default = "bitcoin";
+        description = "The user as which to run bitcoind.";
+      };
+
+      group = mkOption {
+        type = types.str;
+        default = cfg.user;
+        description = "The group as which to run bitcoind.";
+      };
 
       package = mkOption {
         type = types.package;
@@ -63,129 +105,134 @@ in {
         defaultText = "pkgs.altcoins.bitcoind";
         description = "The package providing bitcoin binaries.";
       };
-      extraConfig = mkOption {
-        type = types.lines;
-        default = "";
-        example = ''
-          par=16
-          rpcthreads=16
-          logips=1
-        '';
-        description = "Additional configurations to be appended to <filename>bitcoin.conf</filename>.";
-      };
-      dataDir = mkOption {
-        type = types.path;
-        default = "/var/lib/bitcoind";
-        description = "The data directory for bitcoind.";
-      };
 
-      user = mkOption {
-        type = types.str;
-        default = "bitcoin";
-        description = "The user as which to run bitcoind.";
-      };
-      group = mkOption {
-        type = types.str;
-        default = cfg.user;
-        description = "The group as which to run bitcoind.";
-      };
+      networks = mkOption {
+        default = {};
 
-      rpc = {
-        port = mkOption {
-          type = types.nullOr types.port;
-          default = null;
-          description = "Override the default port on which to listen for JSON-RPC connections.";
-        };
-        users = mkOption {
-          default = {};
-          example = {
-            alice.passwordHMAC = "f7efda5c189b999524f151318c0c86$d5b51b3beffbc02b724e5d095828e0bc8b2456e9ac8757ae3211a5d9b16a22ae";
-            bob.passwordHMAC = "b2dd077cb54591a2f3139e69a897ac$4e71f08d48b4347cf8eff3815c0e25ae2e9a4340474079f55705f40574f4ec99";
-          };
-          type = with types; loaOf (submodule rpcUserOpts);
-          description = ''
-            RPC user information for JSON-RPC connnections.
-          '';
-        };
-      };
-
-      testnet = mkOption {
-        type = types.bool;
-        default = false;
-        description = "Whether to use the test chain.";
-      };
-      port = mkOption {
-        type = types.nullOr types.port;
-        default = null;
-        description = "Override the default port on which to listen for connections.";
-      };
-      dbCache = mkOption {
-        type = types.nullOr (types.ints.between 4 16384);
-        default = null;
-        example = 4000;
-        description = "Override the default database cache size in megabytes.";
-      };
-      prune = mkOption {
-        type = types.nullOr (types.coercedTo
-          (types.enum [ "disable" "manual" ])
-          (x: if x == "disable" then 0 else 1)
-          types.ints.unsigned
-        );
-        default = null;
-        example = 10000;
         description = ''
-          Reduce storage requirements by enabling pruning (deleting) of old
-          blocks. This allows the pruneblockchain RPC to be called to delete
-          specific blocks, and enables automatic pruning of old blocks if a
-          target size in MiB is provided. This mode is incompatible with -txindex
-          and -rescan. Warning: Reverting this setting requires re-downloading
-          the entire blockchain. ("disable" = disable pruning blocks, "manual"
-          = allow manual pruning via RPC, >=550 = automatically prune block files
-          to stay under the specified target size in MiB)
+          Each attribute of this option defines a systemd user service that runs a
+          bitcoin node. The name of each systemd service is
+          <literal>bitcoind-<replaceable>network</replaceable>.service</literal>,
+          where <replaceable>network</replaceable> is the corresponding attribute
+          name.
         '';
+
+        example = ''
+          testnet = {
+            testnet = true;
+            dataDir = "/var/lib/bitcoin";
+            prune = 1000;
+            extraConfig = '''
+              rpcuser=rpcuser
+              rpcpassword=rpcpass
+            ''';
+          };
+
+          mainnet = {
+            dataDir = "/var/lib/bitcoin-testnet";
+            prune = 1000;
+            extraConfig = '''
+              rpcuser=rpcuser
+              rpcpassword=rpcpass
+            ''';
+          };
+        '';
+
+        type = types.attrsOf (types.submodule {
+          options = {
+
+            extraConfig = mkOption {
+              type = types.lines;
+              default = "";
+              example = ''
+                par=16
+                rpcthreads=16
+                logips=1
+              '';
+              description = "Additional configurations to be appended to <filename>bitcoin.conf</filename>.";
+            };
+
+            testnet = mkOption {
+              type = types.bool;
+              default = false;
+              description = "Whether to use the test chain.";
+            };
+
+            dataDir = mkOption {
+              type = types.path;
+              default = "/var/lib/bitcoind";
+              description = "The data directory for bitcoind.";
+            };
+
+            rpc = {
+              port = mkOption {
+                type = types.nullOr types.port;
+                default = null;
+                description = "Override the default port on which to listen for JSON-RPC connections.";
+              };
+              users = mkOption {
+                default = {};
+                example = {
+                  alice.passwordHMAC = "f7efda5c189b999524f151318c0c86$d5b51b3beffbc02b724e5d095828e0bc8b2456e9ac8757ae3211a5d9b16a22ae";
+                  bob.passwordHMAC = "b2dd077cb54591a2f3139e69a897ac$4e71f08d48b4347cf8eff3815c0e25ae2e9a4340474079f55705f40574f4ec99";
+                };
+                type = with types; loaOf (submodule rpcUserOpts);
+                description = ''
+                  RPC user information for JSON-RPC connnections.
+                '';
+              };
+            };
+
+            port = mkOption {
+              type = types.nullOr types.port;
+              default = null;
+              description = "Override the default port on which to listen for connections.";
+            };
+            dbCache = mkOption {
+              type = types.nullOr (types.ints.between 4 16384);
+              default = null;
+              example = 4000;
+              description = "Override the default database cache size in megabytes.";
+            };
+            prune = mkOption {
+              type = types.nullOr (types.coercedTo
+                (types.enum [ "disable" "manual" ])
+                (x: if x == "disable" then 0 else 1)
+                types.ints.unsigned
+              );
+              default = null;
+              example = 10000;
+              description = ''
+                Reduce storage requirements by enabling pruning (deleting) of old
+                blocks. This allows the pruneblockchain RPC to be called to delete
+                specific blocks, and enables automatic pruning of old blocks if a
+                target size in MiB is provided. This mode is incompatible with -txindex
+                and -rescan. Warning: Reverting this setting requires re-downloading
+                the entire blockchain. ("disable" = disable pruning blocks, "manual"
+                = allow manual pruning via RPC, >=550 = automatically prune block files
+                to stay under the specified target size in MiB)
+              '';
+            };
+          };
+        });
       };
     };
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf (cfg.networks != {}) {
     environment.systemPackages = [ cfg.package ];
-    systemd.services.bitcoind = {
-      description = "Bitcoin daemon";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      preStart = ''
-        if ! test -e ${cfg.dataDir}; then
-          mkdir -m 0770 -p '${cfg.dataDir}'
-          chown '${cfg.user}:${cfg.group}' '${cfg.dataDir}'
-        fi
-        ln -sf '${configFile}' '${cfg.dataDir}/bitcoin.conf'
-      '';
-      serviceConfig = {
-        User = cfg.user;
-        Group = cfg.group;
-        ExecStart = "${cfg.package}/bin/bitcoind ${cmdlineOptions}";
-        StateDirectory = "bitcoind";
-        PIDFile = pidFile;
-        Restart = "on-failure";
 
-        # Hardening measures
-        PrivateTmp = "true";
-        ProtectSystem = "full";
-        NoNewPrivileges = "true";
-        PrivateDevices = "true";
-        MemoryDenyWriteExecute = "true";
+    systemd.services =
+      listToAttrs (mapAttrsFlatten (name: value:
+        nameValuePair "bitcoind-${name}" (mkNode value name)) cfg.networks);
 
-        # Permission for preStart
-        PermissionsStartOnly = "true";
-      };
-    };
     users.users.${cfg.user} = {
       name = cfg.user;
       uid  = config.ids.uids.bitcoin;
       group = cfg.group;
       description = "Bitcoin daemon user";
-      home = cfg.dataDir;
     };
+
     users.groups.${cfg.group} = {
       name = cfg.group;
       gid = config.ids.gids.bitcoin;
