1a872c0c9132a5fdd6ad54c1ea1e278130c23a8d
William Casarin <jb55@jb55.com>
2019-01-24 22:34:46 -0800

    spirv-tools: pass in spirv-headers
    Signed-off-by: William Casarin <jb55@jb55.com>

--- pkgs/development/tools/spirv-tools/default.nix
+++ pkgs/development/tools/spirv-tools/default.nix
@@ -1,38 +1,20 @@
-{ stdenv, fetchFromGitHub, cmake, python }:
-
-let
-
-spirv_sources = {
-  # `glslang` requires a specific version of `spirv-tools` and `spirv-headers` as specified in `known-good.json`.
-  tools = fetchFromGitHub {
-    owner = "KhronosGroup";
-    repo = "SPIRV-Tools";
-    rev = "9bfe0eb25e3dfdf4f3fd86ab6c0cda009c9bd661";
-    sha256 = "1spfii4zib1lmz40vnz1d1cr6is7q2n7rvar1vfzi33gwrn8rqhi";
-  };
-  headers = fetchFromGitHub {
-    owner = "KhronosGroup";
-    repo = "SPIRV-Headers";
-    rev = "d5b2e1255f706ce1f88812217e9a554f299848af";
-    sha256 = "18530mpa030ckjhn78z9vbfd35l5jgh3mg22ra6k8gw8zx4wjhsl";
-  };
-};
-
-in
+{ stdenv, spirv-headers, fetchFromGitHub, cmake, python }:
 
 stdenv.mkDerivation rec {
   name = "spirv-tools-${version}";
-  version = "2018-09-20";
+  version = "git-2019-01-08";
 
-  src = spirv_sources.tools;
-  patchPhase = ''ln -sv ${spirv_sources.headers} external/spirv-headers'';
+  src = fetchFromGitHub {
+    owner = "KhronosGroup";
+    repo = "SPIRV-Tools";
+    rev = "df5bd2d05ac1fd3ec3024439f885ec21cc949b22";
+    sha256 = "0l8ds4nn2qcfi8535ai8891i3547x35hscs2jxwwq6qjgw1sgkax";
+  };
+
+  patchPhase = "ln -sv ${spirv-headers} external/spirv-headers";
   enableParallelBuilding = true;
 
-  buildInputs = [ cmake python ];
-
-  passthru = {
-    headers = spirv_sources.headers;
-  };
+  buildInputs = [ cmake python spirv-headers ];
 
   meta = with stdenv.lib; {
     inherit (src.meta) homepage;
