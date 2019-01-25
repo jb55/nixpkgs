5bbe789c472d82e1c59b23c26f8ecb1a685bd975
William Casarin <jb55@jb55.com>
2019-01-24 22:35:54 -0800

    vulkan-headers: 1.1.85 -> 1.1.97
    Signed-off-by: William Casarin <jb55@jb55.com>

--- pkgs/development/libraries/vulkan-headers/default.nix
+++ pkgs/development/libraries/vulkan-headers/default.nix
@@ -1,15 +1,15 @@
 { stdenv, fetchFromGitHub, cmake }:
 stdenv.mkDerivation rec {
   name = "vulkan-headers-${version}";
-  version = "1.1.85";
+  version = "1.1.97";
 
   buildInputs = [ cmake ];
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "Vulkan-Headers";
-    rev = "2fd5a24ec4a6df303b2155b3f85b6b8c1d56f6c0";
-    sha256 = "0cj4bd396qddh3nxvr7grnpfz89g3sbvm21cx4k3ga52sp1rslpb";
+    rev = "v${version}";
+    sha256 = "1yjwhbnccsmn99q9cb0vdimcpa8bacx1cxndzfxbgqzkckmd9mhc";
   };
 
   meta = with stdenv.lib; {
