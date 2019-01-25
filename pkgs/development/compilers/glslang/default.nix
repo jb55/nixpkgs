5f3cd72da9e26b7c99250d5d5ad438fc32dde693
William Casarin <jb55@jb55.com>
2019-01-24 22:35:12 -0800

    glslang: 2018-09-26 -> 2019-01-24
    Signed-off-by: William Casarin <jb55@jb55.com>

--- pkgs/development/compilers/glslang/default.nix
+++ pkgs/development/compilers/glslang/default.nix
@@ -1,14 +1,14 @@
-{ stdenv, fetchFromGitHub, fetchpatch, cmake, bison, spirv-tools, jq }:
+{ stdenv, fetchFromGitHub, fetchpatch, cmake, bison, spirv-tools, spirv-headers, jq }:
 
 stdenv.mkDerivation rec {
   name = "glslang-git-${version}";
-  version = "2018-09-26";
+  version = "2019-01-24";
 
   src = fetchFromGitHub {
     owner = "KhronosGroup";
     repo = "glslang";
-    rev = "91ac4290bcf2cb930b4fb0981f09c00c0b6797e1";
-    sha256 = "0q477pm0n495acnss16ddlf82a6i5l2dfmvc7r8yi0bgmgpzi4av";
+    rev = "2cf6613a0d35545812c34a633ec0772c0320108c";
+    sha256 = "08cvkgw3nxnhp28i44gy4i560jb9iaa1g05x7ck70ak4fs3b0h0s";
   };
 
   buildInputs = [ cmake bison jq ] ++ spirv-tools.buildInputs;
@@ -16,13 +16,13 @@ stdenv.mkDerivation rec {
 
   postPatch = ''
     cp --no-preserve=mode -r "${spirv-tools.src}" External/spirv-tools
-    ln -s "${spirv-tools.headers}" External/spirv-tools/external/spirv-headers
+    ln -s "${spirv-headers}" External/spirv-tools/external/spirv-headers
   '';
 
   preConfigure = ''
     HEADERS_COMMIT=$(jq -r < known_good.json '.commits|map(select(.name=="spirv-tools/external/spirv-headers"))[0].commit')
     TOOLS_COMMIT=$(jq -r < known_good.json '.commits|map(select(.name=="spirv-tools"))[0].commit')
-    if [ "$HEADERS_COMMIT" != "${spirv-tools.headers.rev}" ] || [ "$TOOLS_COMMIT" != "${spirv-tools.src.rev}" ]; then
+    if [ "$HEADERS_COMMIT" != "${spirv-headers.src.rev}" ] || [ "$TOOLS_COMMIT" != "${spirv-tools.src.rev}" ]; then
       echo "ERROR: spirv-tools commits do not match expected versions: expected tools at $TOOLS_COMMIT, headers at $HEADERS_COMMIT";
       exit 1;
     fi
