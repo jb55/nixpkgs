8d6ceeea47aee7ad4e7bdd437b4280d3e9a95304
William Casarin <jb55@jb55.com>
2019-04-18 16:39:53 -0700

    gixy: disable for now

--- pkgs/build-support/writers/default.nix
+++ pkgs/build-support/writers/default.nix
@@ -245,7 +245,6 @@ rec {
   } /* sh */ ''
     # nginx-config-formatter has an error - https://github.com/1connect/nginx-config-formatter/issues/16
     awk -f ${awkFormatNginx} "$textPath" | sed '/^\s*$/d' > $out
-    gixy $out
   '';
 
   # writePerl takes a name an attributeset with libraries and some perl sourcecode and
