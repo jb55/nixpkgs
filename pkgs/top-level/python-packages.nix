1e1680bb57b6408e06de0e59f2a9f71cff29b70e
William Casarin <jb55@jb55.com>
2019-09-08 06:24:04 -0700

    pylightning: init at 0.0.7.3
    Needed for clightning plugins
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5314,6 +5314,8 @@ in {
 
   pylibmc = callPackage ../development/python-modules/pylibmc {};
 
+  pylightning = callPackage ../development/python-modules/pylightning { };
+
   pymetar = callPackage ../development/python-modules/pymetar { };
 
   pypubsub = callPackage ../development/python-modules/pypubsub { };
