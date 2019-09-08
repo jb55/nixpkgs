f535e50ab8d8f2de58edd25616df2449b3309b06
William Casarin <jb55@jb55.com>
2019-09-08 06:24:04 -0700

    pylightning: init at 0.0.7.3
    Needed for clightning plugins
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4868,6 +4868,8 @@ in {
 
   pylibmc = callPackage ../development/python-modules/pylibmc {};
 
+  pylightning = callPackage ../development/python-modules/pylightning { };
+
   pymetar = callPackage ../development/python-modules/pymetar { };
 
   pysftp = callPackage ../development/python-modules/pysftp { };
