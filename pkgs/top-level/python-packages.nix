438e01d02b643290e80c7124832f91615c6e91f3
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-08-31 02:25:38 +0200

    python3Packages.diffusers: init at 0.2.4
    https://github.com/huggingface/diffusers
    State-of-the-art diffusion models for image and audio generation in PyTorch

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2359,6 +2359,8 @@ self: super: with self; {
 
   diff-match-patch = callPackage ../development/python-modules/diff-match-patch { };
 
+  diffusers = callPackage ../development/python-modules/diffusers { };
+
   digital-ocean = callPackage ../development/python-modules/digitalocean { };
 
   digi-xbee = callPackage ../development/python-modules/digi-xbee { };
